58dfaf82c27d91f5c8d71b85bc6faec5caccfae0
Andy Pohl <andy.pohl@wisc.edu>
2017-02-22 23:48:53 -0600

    disabling for now

--- chtc/__init__.py
+++ chtc/__init__.py
@@ -2,7 +2,7 @@
 from chtc.docopt import docopt
 
 # functions dealing with software JSON file
-from chtc.softjson import tarball_prefix, load_software_db, find_all_software, list_software
+#from chtc.softjson import tarball_prefix, load_software_db, find_all_software, list_software
 
 # class to make the shell script
 from chtc.shellscript import ShellScript
