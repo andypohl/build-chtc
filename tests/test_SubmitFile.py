f420ee7aebf2ed84fa66d8f6b8744d2f78ae9914
Andy Pohl <andy.pohl@wisc.edu>
2017-03-07 16:39:30 -0600

    new submit file tests

---
+++ tests/test_SubmitFile.py
@@ -0,0 +1,31 @@
+'''A couple tests for SubmitFile class.'''
+
+import unittest2 as unittest
+#import unittest
+import sys
+from chtc import SubmitFile
+
+class TestSubmitFile(unittest.TestCase):
+    '''Tests for SubmitFile'''
+    def setUp(self):
+        # Don't initialize this with a regular python dict if you want to 
+        # preserve ordering!
+        self.sub = SubmitFile("mysoft")
+
+    def test_queue_is_last(self):
+        '''Make sure queue line is last.'''
+        keys = self.sub.keys()
+        self.assertEqual(keys[-1], "queue")
+
+    def test_locked(self):
+        '''Make sure we can't add lines.'''
+        self.assertRaises(KeyError, self.sub.__setitem__, 'newline', 'newvalue')
+
+    def test_can_change_specific_lines(self):
+        '''We can change lines though.'''
+        self.assertEquals(self.sub['request_memory'], '1GB')
+        self.sub['request_memory'] = '4GB'
+        self.assertEquals(self.sub['request_memory'], '4GB')
+
+    def tearDown(self):
+        del self.sub
