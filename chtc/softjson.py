83afc1cacb616e2ef6dc86b7ddb0852655ebd3e8
Andy Pohl <andy.pohl@wisc.edu>
2017-05-09 22:09:34 -0500

    Recursive to BFS change + test

--- chtc/softjson.py
+++ chtc/softjson.py
@@ -20,31 +20,38 @@ class SoftJson(object):
                 self.software[prefix] = soft_entry
                 self.defaults[soft_entry['software']] = prefix
 
-    def get_deps_recursive(self, prefix):
-        '''Recurse over a definitive list of dependent software and skip ones seen'''
+    def get_deps_bfs(self, prefix):
+        '''BF-traverse over a definitive list of dependent software and prioritize ones seen'''
         if prefix not in self.software:
             raise LookupError("Couldn't find software %s in the database")
-        software = self.software[prefix]
+        top_software = self.software[prefix]
+        # Working queue (FIFO)
+        soft_queue = []
+        soft_queue.append(top_software)
+        # Final list to construct (LIFO)
         soft_list = []
-        if 'dependent_software' in software:
-            for dep in software['dependent_software']:
-                dep_soft_list = self.get_deps_recursive(dep.get_prefix())
-                # dependencies of dependencies might be asking for a different version
-                # so we need to account for that.  Building two different versions of a 
-                # software is even worse than building one version twice.
-                versionless_list = [d['software'] for d in dep_soft_list]
-                for dep_soft in dep_soft_list:
-                    vl = dep_soft['software']
-                    if vl not in versionless_list:
-                        soft_list.append(dep_soft)
-        soft_list.append(software)
+        while len(soft_queue) > 0:
+            # pop the top of the queue
+            # python 2.7 dequeue and collections would be more elegant
+            popped = soft_queue.pop(0)
+            # version-less check to see if software's in the list already or not
+            version_less_list = [t['software'] for t in soft_list]
+            if popped['software'] in version_less_list:
+                # if it is, pull that one off the list so it gets compiled earlier
+                removed_ix = version_less_list.index(popped['software'])
+                removed = soft_list.pop(removed_ix)
+                popped = removed
+            soft_list.insert(0, popped)
+            if 'dependent_software' in popped:
+                for dep in popped['dependent_software']:
+                    soft_queue.append(self.software[dep.get_prefix()])
         return soft_list
 
-    def build_commands_recursive(self, prefix):
-        '''Inner recursive function which needs reversing in an outer function.'''
+    def commands_cat(self, prefix):
+        '''Inner function which needs reversing in an outer function.'''
         if prefix not in self.software:
             raise LookupError("Couldn't find software %s in the database")
-        soft_list = self.get_deps_recursive(prefix)
+        soft_list = self.get_deps_bfs(prefix)
         commands_array = []
         for soft in soft_list:
             commands_array.extend(self.software[soft.get_prefix()]['build_commands'])
@@ -62,6 +69,10 @@ class SoftJson(object):
             except KeyError:
                 raise LookupError("Couldn't find %s in software database" %(software_name))
         return prefix
+
+    def subset(self, prefix_list):
+        '''Return all or some of the SoftEntrys as a list'''
+        return [self.software[prefix] for prefix in prefix_list]
     
     def required_substitutions(self, prefix):
         '''Lookup a substitution list given a prefix'''
